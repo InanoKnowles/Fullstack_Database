3bb95add5ecebf04113c247d3e55f5a65501d2a0
inanok <missinanoknowles@gmail.com>
2022-12-14 10:52:51 +1000

    Update: Added the ability to add tasks and assign people to those tasks

---
+++ commandboard/db/schema.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE command_board_db;
+\c command_board_db
+
+-- Tables needed:
+
+--> For the tasks being made and assigned
+CREATE TABLE tasks(
+  id SERIAL PRIMARY KEY,
+  task_name TEXT,
+  task_instructions TEXT,
+  assign_task_to TEXT
+);
+
+-- In case of emergencies:
+DROP TABLE tasks;
+
+-- Add items to the tasks table
+INSERT INTO tasks(task_name, task_instructions, assign_task_to)
+VALUES
+('Laundry', 'Take all the dirty laundry and put through a wash cycle in the the washing machine. Hang up to dry and then bring back inside once all cleaned and dry.', 'Mario'),
+('Dishes', 'Collect all the dirty dishes around the house, wash, dry and put away into designated spots.', 'Luigi');
+
+--> That looks like this on psql:
+--  id | task_name |                                                                    task_instructions                                                                    | assign_task_to 
+-- ----+-----------+---------------------------------------------------------------------------------------------------------------------------------------------------------+----------------
+--   1 | Laundry   | Take all the dirty laundry and put through a wash cycle in the the washing machine. Hang up to dry and then bring back inside once all cleaned and dry. | Mario
+--   2 | Dishes    | Collect all the dirty dishes around the house, wash, dry and put away into designated spots.                                                            | Luigi
+-- (2 rows)
+-- ----+-----------+---------------------------------------------------------------------------------------------------------------------------------------------------------+----------------
+
+--> For the users of the application
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  dob INTEGER,
+  image_url TEXT
+);
--- commandboard/main.rb
+++ commandboard/main.rb
@@ -1,11 +1,33 @@
-     
 require 'sinatra'
+require 'pg'
 
-get '/' do
-  erb :index
+def run_sql(sql, sql_params = [])
+  db = PG.connect(dbname: 'command_board_db')
+  results = db.exec_params(sql, sql_params)
+  db.close
+  results
 end
 
+get '/' do
+  tasks = run_sql('SELECT * FROM tasks')
 
+  erb :'tasks/index', locals: {
+    tasks: tasks
+  }
+end
 
+get '/tasks/new' do
+  # We ONLY use erb files if its a GET request.
+  erb :'tasks/new'
+end
 
+post '/tasks' do
+  task_name = params['task_name']
+  task_instructions = params['task_instructions']
+  assign_task_to = params['assign_task_to']
 
+  run_sql("INSERT INTO tasks(task_name, task_instructions, assign_task_to) VALUES($1, $2, $3)", [task_name, task_instructions, assign_task_to])
+
+  # We ALWAYS redirect the user at the end of a POST, PUT or DELETE method.
+  redirect '/'
+end
