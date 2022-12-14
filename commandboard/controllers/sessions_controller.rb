4eb2f7b96cb61a154491926cbf41e2384ebff7a4
inanok <missinanoknowles@gmail.com>
2022-12-14 17:06:44 +1000

    All basics are done. Just need to add a feature to make it hit MVP

--- commandboard/controllers/sessions_controller.rb
+++ commandboard/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 get '/sessions/new' do
-    erb :'sessions/new'
-  end
+  erb :'sessions/new'
+end
   
-  post '/sessions' do
+post '/sessions' do
     email = params['email']
     password = params['password']
-  
     user = find_user_by_email(email)
-    if user && BCrypt::Password.new(user['password_digest']) == password
+
+  if user && BCrypt::Password.new(user['password_digest']) == password
       session['user_id'] = user['id']
       redirect '/'
     else
       erb :'sessions/new'
-    end
   end
+end
   
-  delete '/sessions' do
+delete '/sessions' do
     session['user_id'] = nil
     redirect '/'
-  end
+end
