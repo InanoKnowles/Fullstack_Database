58123256d7d66d4fb78bf28b1400da6689302c91
inanok <missinanoknowles@gmail.com>
2022-12-16 11:45:44 +1000

    fixed sign up issue

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -7,6 +7,6 @@ post '/users' do
   last_name = params['last_name']
   email = params['email']
   password = params['password']
-  create_user(first_name, last_name, email,)
+  create_user(first_name, last_name, email, password)
   redirect '/'
 end
